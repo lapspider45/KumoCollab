07279132850fb2454c7e125d99dd2510865de3b7
lapspider45 <70412163+lapspider45@users.noreply.github.com>
2021-12-13 21:54:37 +0200

    add timer functionality to bullets, will be used later

--- game/bullets/SimpleBullet.gd
+++ game/bullets/SimpleBullet.gd
@@ -2,6 +2,8 @@
 class_name SimpleBullet
 extends Sprite
 
+signal tick
+
 export var collision_radius := 1.0
 export var radius := 1.0
 export var velocity:Vector2
@@ -17,3 +19,11 @@ func advance(delta):
 	velocity += acceleration * delta
 	translate(velocity * delta)
 	lifetime -= delta
+	emit_signal("tick")
+
+
+func Timer(time:float):
+	var end_time = lifetime - time
+	assert(end_time > 0.0, "Timer must complete before bullet times out")
+	while lifetime > end_time:
+		yield(self, "tick")
